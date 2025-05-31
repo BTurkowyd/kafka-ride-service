233f36d781768641602f0c889ec54d4ab44f52ef
Bartosz Turkowyd <vertax@gmail.com>
2025-05-31 15:12:15 +0200

    #18: trying to figure out a producer-kafka connection

--- producer/producer.py
+++ producer/producer.py
@@ -24,8 +24,7 @@ KAFKA_BROKER = os.getenv("KAFKA_BROKER", "localhost:9092")
 # Producer config (common)
 producer_config = {
     "bootstrap.servers": KAFKA_BROKER,
-    "key.serializer": StringSerializer("utf_8"),
-    "debug": "all"
+    "key.serializer": StringSerializer("utf_8")
 }
 
 # Load known users
